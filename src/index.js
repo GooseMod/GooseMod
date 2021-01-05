7f1b235f2bd6dd1bd5bf22de9d80fefa96a7938a
Oj18 <oj@oojmed.com>
2021-01-05 17:33:32 +0000

    Bump version, remove version toast on inject

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ const init = async function () {
   this.disabledModules = {};
 
   this.lastVersion = localStorage.getItem('goosemodLastVersion');
-  this.version = '5.6.0';
+  this.version = '5.7.0';
   this.versionHash = '<hash>'; // Hash of built final js file is inserted here via build script
 
   fetch('https://goosemod-api.netlify.app/injectVersion.json').then((x) => x.json().then((latestInjectVersionInfo) => {
@@ -128,9 +128,7 @@ const init = async function () {
     
     // delete window.gmUntethered;
   }
-  
-  this.showToast(`GooseMod v${this.version} (${this.versionHash.substring(0, 7)})`, {timeout: 1000});
-  
+
   this.messageEasterEggs.interval = setInterval(this.messageEasterEggs.check, 1000);
   
   this.saveInterval = setInterval(this.saveModuleSettings, 3000);
