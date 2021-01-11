819e8b0f12a81964cbe9e6c5109f5be7ba17148a
Oj18 <oj@oojmed.com>
2021-01-11 20:22:12 +0000

    [Patcher API] New MiniPopover adding API

--- src/util/patcher/index.js
+++ src/util/patcher/index.js
@@ -1,9 +1,12 @@
 export { patch, inject, uninject } from './base';
 
 import * as _contextMenu from './contextMenu';
+import * as _miniPopover from './miniPopover';
 
 export const contextMenu = _contextMenu;
+export const miniPopover = _miniPopover;
 
 export const setThisScope = (scope) => {
   _contextMenu.setThisScope(scope);
+  _miniPopover.setThisScope(scope);
 };
---
+++ src/util/patcher/miniPopover.js
@@ -0,0 +1,48 @@
+import { patch } from './base';
+import { findInReactTree } from '../react';
+
+let goosemodScope = {};
+
+export const setThisScope = (scope) => {
+  goosemodScope = scope;
+};
+
+export const add = (tooltipText, svgSrc, clickHandler) => {
+  const { React } = goosemodScope.webpackModules.common;
+  const Tooltip = goosemodScope.webpackModules.findByDisplayName('Tooltip');
+  const { icon: iconClass } = goosemodScope.webpackModules.findByProps('icon', 'isHeader');
+  
+  const MiniPopover = goosemodScope.webpackModules.find((m) => m.default && m.default.displayName === 'MiniPopover')
+  
+  return patch(MiniPopover, 'default', (args, res) => {
+    const props = findInReactTree(res, (r) => r && r.message);
+    if (!props) return res;
+
+    res.props.children.unshift(
+      React.createElement(Tooltip, {
+        postion: "top",
+        text: tooltipText
+      }, ({
+        onMouseLeave,
+        onMouseEnter
+      }) =>
+        React.createElement(MiniPopover.Button, {
+          onClick: () => {
+            clickHandler(props);
+          },
+          onMouseEnter: onMouseEnter,
+          onMouseLeave: onMouseLeave
+        },
+          React.createElement("img", {
+            src: svgSrc,
+            width: "24px",
+            height: "24px",
+            className: iconClass
+          })
+        )
+      )
+    );
+
+    return res;
+  });
+};
