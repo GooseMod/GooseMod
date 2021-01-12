26886ed0ec16de69cf4fa290cb14b905410ba005
Oj18 <oj@oojmed.com>
2021-01-12 18:00:43 +0000

    [Patcher API] Channel Text Area Buttons: Added new API

---
+++ src/util/patcher/channelTextAreaButtons.js
@@ -0,0 +1,56 @@
+import * as PatcherBase from './base';
+import { findInReactTree } from '../react';
+
+let goosemodScope = {};
+
+export const setThisScope = (scope) => {
+  goosemodScope = scope;
+};
+
+export const patch = (tooltipText, imgSrc, clickHandler) => {
+  const { React } = goosemodScope.webpackModules.common;
+  const Tooltip = goosemodScope.webpackModules.findByDisplayName('Tooltip');
+  const Button = goosemodScope.webpackModules.findByProps('Looks', 'DropdownSizes');
+
+  const buttonClasses = goosemodScope.webpackModules.findByProps('button');
+  const buttonWrapperClasses = goosemodScope.webpackModules.findByProps('buttonWrapper', 'pulseButton');
+  const buttonTextAreaClasses = goosemodScope.webpackModules.findByProps('button', 'textArea');
+
+  const ChannelTextAreaContainer = goosemodScope.webpackModules.find(m => m.type && m.type.render && m.type.render.displayName === 'ChannelTextAreaContainer');
+
+  return PatcherBase.patch(ChannelTextAreaContainer.type, 'render', (_args, res) => {
+    const props = findInReactTree(res, (r) => r && r.className && r.className.indexOf("buttons-") === 0);
+    if (!props) return res;
+
+    props.children.unshift(
+      React.createElement('div', null,
+        React.createElement(Tooltip, {
+          postion: "top",
+          text: tooltipText
+        }, ({
+          onMouseLeave,
+          onMouseEnter
+        }) =>
+          React.createElement(Button, {
+            look: Button.Looks.BLANK,
+            size: Button.Sizes.ICON,
+            onClick: () => {
+              clickHandler(props);
+            },
+            onMouseEnter,
+            onMouseLeave
+          },
+            React.createElement("img", {
+              src: imgSrc,
+              width: "24px",
+              height: "24px",
+              className: `${buttonTextAreaClasses.button} ${buttonClasses.contents} ${buttonWrapperClasses.button}`
+            })
+          )
+        )
+      )
+    );
+
+    return res;
+  });
+};
--- src/util/patcher/index.js
+++ src/util/patcher/index.js
@@ -2,11 +2,14 @@ export { patch, inject, uninject } from './base';
 
 import * as _contextMenu from './contextMenu';
 import * as _miniPopover from './miniPopover';
+import * as _channelTextAreaButtons from './channelTextAreaButtons';
 
 export const contextMenu = _contextMenu;
 export const miniPopover = _miniPopover;
+export const channelTextAreaButtons = _channelTextAreaButtons;
 
 export const setThisScope = (scope) => {
   _contextMenu.setThisScope(scope);
   _miniPopover.setThisScope(scope);
+  _channelTextAreaButtons.setThisScope(scope);
 };
