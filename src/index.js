601e3cd72f2f821bcb3060a7860620db546fb578
Oj18 <oj@oojmed.com>
2021-01-14 09:16:58 +0000

    [Chore] Bump version: 6.0.0-dev-rc1 -> 6.0.0-dev-rc-2

--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ const init = async function () {
   this.disabledModules = {};
 
   this.lastVersion = localStorage.getItem('goosemodLastVersion');
-  this.version = '6.0.0-dev-rc1';
+  this.version = '6.0.0-dev-rc-2';
   this.versionHash = '<hash>'; // Hash of built final js file is inserted here via build script
 
   fetch('https://goosemod-api.netlify.app/injectVersion.json').then((x) => x.json().then((latestInjectVersionInfo) => {
