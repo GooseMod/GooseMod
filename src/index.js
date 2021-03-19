beff3d1d8434fb2c7e5c7853cf464476b0713705
Oj18 <oj@oojmed.com>
2021-03-19 21:25:56 +0000

    [Release Prep] v7.2.0-dev -> v7.2.0

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ const init = async function () {
   this.disabledModules = {};
 
   this.lastVersion = localStorage.getItem('goosemodLastVersion');
-  this.version = '7.2.0-dev';
+  this.version = '7.2.0';
   this.versionHash = '<hash>'; // Hash of built final js file is inserted here via build script
 
   fetch(`${this.moduleStoreAPI.apiBaseURL}/injectVersion.json`).then((x) => x.json().then((latestInjectVersionInfo) => {
