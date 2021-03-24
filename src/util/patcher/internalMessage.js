e1ee80becf5adab9d124947fed3afa3ae0a5e326
Oj18 <oj@oojmed.com>
2021-03-24 21:55:40 +0000

    [Patcher > Internal Message] Initial Add

---
+++ src/util/patcher/internalMessage.js
@@ -0,0 +1,29 @@
+let goosemodScope = {};
+
+export const setThisScope = (scope) => {
+  goosemodScope = scope;
+
+  const { BOT_AVATARS } = goosemodScope.webpackModules.findByProps('BOT_AVATARS', 'DEFAULT_AVATARS');
+
+  BOT_AVATARS.GooseMod = 'https://cdn.discordapp.com/avatars/760559484342501406/5125aff2f446ad7c45cf2dfd6abf92ed.webp'; // Add avatar image
+};
+
+
+export const send = (content, author = 'GooseMod') => {
+  // Get Webpack Modules
+  const { createBotMessage } = goosemodScope.webpackModules.findByProps('createBotMessage');
+  const { getChannelId } = goosemodScope.webpackModules.findByProps('getChannelId');
+  const { recieveMessage } = goosemodScope.webpackModules.findByProps('receiveMessage', 'sendBotMessage');
+
+  const msg = createBotMessage(getChannelId(), content);
+
+  msg.state = 'SENT'; // Set Clyde-like props
+  msg.author.id = '1';
+  msg.author.bot = true;
+  msg.author.discriminator = '0000';
+
+  msg.author.avatar = 'GooseMod'; // Allow custom avatar URLs in future? (via dynamic BOT_AVATARS adding)
+  msg.author.username = author;
+
+  recieveMessage(getChannelId(), msg);
+};
