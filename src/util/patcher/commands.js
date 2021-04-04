785135088ceb0c8d9c1bb9efff3747f22da56959
Oj18 <oj@oojmed.com>
2021-04-04 21:26:23 +0100

    [Patcher > Commands] Add type to commands added

--- src/util/patcher/commands.js
+++ src/util/patcher/commands.js
@@ -24,7 +24,8 @@ export const add = (name, description, execute, options = []) => {
 
   mod.BUILT_IN_COMMANDS.push({
     applicationId,
-  
+    type: 1,
+
     description,
     name,
     execute,
