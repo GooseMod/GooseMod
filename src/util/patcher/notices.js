f78663cb75811fe51a3d174d4259b4dc1f87d482
Oj18 <oj@oojmed.com>
2021-05-29 20:09:54 +0100

    [Patcher > Notices] Remove extra logging

--- src/util/patcher/notices.js
+++ src/util/patcher/notices.js
@@ -43,7 +43,6 @@ export const setThisScope = async (scope) => {
   }
 
   PatcherBase.patch(baseOwnerInstance.props.children, 'type', (_args, ret) => {
-    console.log(ret);
     ret.props.children[1].props.children.props.children.unshift(React.createElement(NoticeContainer));
 
     return ret;
