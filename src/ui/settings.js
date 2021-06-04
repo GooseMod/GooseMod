93ce0b772f4adb49e456a592063546e6c2f2793a
Oj18 <oj@oojmed.com>
2021-06-04 13:19:38 +0100

    [Home] Use Discord icons for button icons

--- src/ui/settings.js
+++ src/ui/settings.js
@@ -2573,38 +2573,15 @@ const addToHome = () => {
   const ScrollerClasses = goosemodScope.webpackModules.findByProps('scrollerBase', 'auto');
   const HomeMiscClasses = goosemodScope.webpackModules.findByProps('headerBarContainer', 'pageContent');
 
-  const ThemesIcon = React.createElement('svg', {
-    viewBox: '0 0 24 24',
-    height: 24,
+  const ThemesIcon = React.createElement(goosemodScope.webpackModules.findByDisplayName('Eye'), {
     width: 24,
-    fill: 'none',
-    'aria-hidden': 'false'
-  },
-    React.createElement('path', {
-      fill: 'currentColor',
+    height: 24
+  })
 
-      d: 'M15.5 12a3.5 3.5 0 1 1-7 0a3.5 3.5 0 0 1 7 0z',
-    }),
-    React.createElement('path', {
-      fillRule: 'evenodd',
-      fill: 'currentColor',
-
-      d: 'M12 3.5c-3.432 0-6.125 1.534-8.054 3.24C2.02 8.445.814 10.352.33 11.202a1.6 1.6 0 0 0 0 1.598c.484.85 1.69 2.758 3.616 4.46C5.876 18.966 8.568 20.5 12 20.5c3.432 0 6.125-1.534 8.054-3.24c1.926-1.704 3.132-3.611 3.616-4.461a1.6 1.6 0 0 0 0-1.598c-.484-.85-1.69-2.757-3.616-4.46C18.124 5.034 15.432 3.5 12 3.5zM1.633 11.945c.441-.774 1.551-2.528 3.307-4.08C6.69 6.314 9.045 5 12 5c2.955 0 5.309 1.315 7.06 2.864c1.756 1.553 2.866 3.307 3.307 4.08a.111.111 0 0 1 .017.056a.111.111 0 0 1-.017.056c-.441.774-1.551 2.527-3.307 4.08C17.31 17.685 14.955 19 12 19c-2.955 0-5.309-1.315-7.06-2.864c-1.756-1.553-2.866-3.306-3.307-4.08A.11.11 0 0 1 1.616 12a.11.11 0 0 1 .017-.055z',
-    })
-  );
-
-  const PluginsIcon = React.createElement('svg', {
-    viewBox: '0 0 24 24',
-    height: 24,
+  const PluginsIcon = React.createElement(goosemodScope.webpackModules.findByDisplayName('InlineCode'), {
     width: 24,
-    fill: 'none',
-    'aria-hidden': 'false'
-  }, React.createElement('path', {
-    fillRule: 'evenodd',
-    fill: 'currentColor',
-
-    d: 'M8.78 4.97a.75.75 0 0 1 0 1.06L2.81 12l5.97 5.97a.75.75 0 1 1-1.06 1.06l-6.5-6.5a.75.75 0 0 1 0-1.06l6.5-6.5a.75.75 0 0 1 1.06 0zm6.44 0a.75.75 0 0 0 0 1.06L21.19 12l-5.97 5.97a.75.75 0 1 0 1.06 1.06l6.5-6.5a.75.75 0 0 0 0-1.06l-6.5-6.5a.75.75 0 0 0-1.06 0z',
-  }));
+    height: 24
+  });
 
   const HeaderBarContainer = goosemodScope.webpackModules.findByDisplayName('HeaderBarContainer');
 
@@ -2631,7 +2608,7 @@ const addToHome = () => {
       id: 'gm-settings-inject',
 
       style: {
-        padding: '16px',
+        padding: '22px',
         backgroundColor: 'var(--background-primary)',
 
         height: '100%',
