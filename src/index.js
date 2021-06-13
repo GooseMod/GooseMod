c3cd75a5b3ec290a59beabbd28a54c778db5f992
Oj18 <oj@oojmed.com>
2021-06-13 18:49:01 +0100

    [Chore] Bump version: v8.11.0-dev

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ const init = async function () {
   }
 
   this.versioning = {
-    version: '8.10.0',
+    version: '8.11.0-dev',
     hash: '<hash>', // Hash of built final js file is inserted here via build script
 
     lastUsedVersion: localStorage.getItem('goosemodLastVersion')
