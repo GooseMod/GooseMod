fed46404f2710297e6ca7627ae77753e9c66146a
Oj18 <oj@oojmed.com>
2021-06-25 22:23:19 +0100

    [Settings > Items] header: Add experimental icon prop

--- src/ui/settings/items/header.js
+++ src/ui/settings/items/header.js
@@ -4,8 +4,47 @@ const FormTitle = goosemod.webpackModules.findByDisplayName('FormTitle');
 
 const Margins = goosemod.webpackModules.findByProps('marginTop20', 'marginBottom20');
 
+const Tooltip = goosemod.webpackModules.findByDisplayName('Tooltip');
+const Science = goosemod.webpackModules.findByDisplayName('Science');
+
 
 export default class Header extends React.PureComponent {
+  constructor(props) {
+    if (props.experimental) {
+      props.text = [
+        React.createElement(Tooltip, {
+          position: 'top',
+          color: 'primary',
+
+          text: 'Experimental',
+        }, ({
+          onMouseLeave,
+          onMouseEnter
+        }) =>
+          React.createElement(Science, {
+            width: 18,
+            height: 18,
+
+            className: 'gm-experimental-label-icon',
+
+            onMouseLeave,
+            onMouseEnter
+          })
+        ),
+
+        React.createElement('span', {
+          style: {
+            verticalAlign: 'unset'
+          },
+
+          className: 'gm-experimental-label-text'
+        }, props.text)
+      ];
+    }
+
+    super(props);
+  }
+
   render() {
     return React.createElement(FormTitle, {
       tag: 'h5',
