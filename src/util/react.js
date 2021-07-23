ad00a8e9a5f0e330457eca131ae1f149e979884d
Oj18 <oj@oojmed.com>
2021-07-23 18:31:34 +0100

    [ReactUtils] Finish rewrite

--- src/util/react.js
+++ src/util/react.js
@@ -1,88 +1,41 @@
-export const getReactInstance = (el) => el && el[Object.keys(el).find((x) =>
+export const getReactInstance = (el) => el && el[Object.keys(el).find((x) => // Get React node from HTML element via internal key
   x.startsWith('__reactInternalInstance') || // Old React
   x.startsWith('__reactFiber$') // New React (in Canary since ~22/07/21)
 )];
 
-// https://rauenzi.github.io/BDPluginLibrary/docs/modules_reacttools.js.html
-export function getOwnerInstance(node, {include, exclude = ["Popout", "Tooltip", "Scroller", "BackgroundFlash"], filter = _ => _} = {}) {
-  if (node === undefined) return undefined;
-  
-  const excluding = include === undefined;
-  const nameFilter = excluding ? exclude : include;
-  
-  function getDisplayName(owner) {
-    const type = owner.type;
-    if (!type) return null;
-    return type.displayName || type.name || null;
+export const getOwnerInstance = (node) => { // Go through React node's parent until one is a React node (not null or HTML element)
+  let inst = getReactInstance(node);
+
+  while (inst.return) {
+    inst = inst.return;
+
+    if (inst.stateNode?._reactInternals) return inst.stateNode;
   }
-  
-  function classFilter(owner) {
-    const name = getDisplayName(owner);
-    return (name !== null && !!(nameFilter.includes(name) ^ excluding));
-  }
-  
-  let curr = getReactInstance(node);
-  
-  for (curr = curr && curr.return; curr !== null; curr = curr.return) {
-    if (curr === null) continue;
-    
-    const owner = curr.stateNode;  
-    if (owner !== null && !(owner instanceof HTMLElement) && classFilter(curr) && filter(owner)) return owner;
-  }
-  
+
   return null;
-}
+};
 
-export function findInReactTree(tree, searchFilter) {
-  return findInTree(tree, searchFilter, {walkable: ["props", "children", "child", "sibling"]});
-}
+export const findInTree = (parent, filter, opts) => { // Find in tree utility function - parameters supported like BD's + PC's APIs to maintain compatibility
+  const { walkable = null, ignore = [] } = opts;
 
-export function findInTree(tree, filter, {walkable = null, ignore = []} = {}) {
-  if (!tree || typeof tree !== 'object') {
+  if (!parent || typeof parent !== 'object') {
     return null;
   }
 
   if (typeof filter === 'string') {
-    if (tree.hasOwnProperty(filter)) {
-      return tree[filter];
-    }
+    if (parent.hasOwnProperty(filter)) return tree[filter];
+    return null;
+  }
+  
+  if (filter(parent)) return parent;
 
-    return;
-  } else if (filter(tree)) {
-    return tree;
+  if (Array.isArray(parent)) {
+    return parent.find((x) => findInTree(x, filter, opts));
   }
 
-  let returnValue = null;
+  return (walkable || Object.keys(parent)).find((x) => !ignore.includes(x) && findInTree(parent[x], filter, opts));
+};
 
-  if (Array.isArray(tree)) {
-    for (const value of tree) {
-      returnValue = findInTree(value, filter, {
-        walkable,
-        ignore
-      });
-
-      if (returnValue) {
-        return returnValue;
-      }
-    }
-  } else {
-    const walkables = !walkable ? Object.keys(tree) : walkable;
-
-    for (const key of walkables) {
-      if (!tree.hasOwnProperty(key) || ignore.includes(key)) {
-        continue;
-      }
-
-      returnValue = findInTree(tree[key], filter, {
-        walkable,
-        ignore
-      });
-
-      if (returnValue) {
-        return returnValue;
-      }
-    }
-  }
-
-  return returnValue;
-}
+export const findInReactTree = (node, filter) => findInTree(node, filter, { // Specialised findInTree for React nodes
+  walkable: [ 'props', 'children', 'child', 'sibling' ]
+});
