a5269933892e0824d0becf5b704fdb67df79c334
Oj18 <oj@oojmed.com>
2021-07-27 12:08:49 +0100

    [CI] build: fix newline in body, tweak name

--- building/generateDevChangelog.sh
+++ building/generateDevChangelog.sh
@@ -2,4 +2,4 @@
 preface="## Commits this release"
 list=git log --pretty="format:%s" | grep -m 1 -B 100 "\[Release Prep\]" | head -n -1 | awk '$0="  - "$0'
 
-echo "$preface\n$list" > dist/devChangelog.md
+echo -en "$preface\n$list" > dist/devChangelog.md
