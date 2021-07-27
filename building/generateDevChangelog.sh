5b16c1fc4e5bde1ca9372372f53284cfdf0b8cbd
Oj18 <oj@oojmed.com>
2021-07-27 12:11:55 +0100

    [CI] build: remove path specific, fix list in body

--- building/generateDevChangelog.sh
+++ building/generateDevChangelog.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 preface="## Commits this release"
-list=git log --pretty="format:%s" | grep -m 1 -B 100 "\[Release Prep\]" | head -n -1 | awk '$0="  - "$0'
+list="$(git log --pretty="format:%s" | grep -m 1 -B 100 "\[Release Prep\]" | head -n -1 | awk '$0="  - "$0')"
 
 echo -en "$preface\n$list" > dist/devChangelog.md
