e8add1cb928103df65afa4585149166f86da280a
Oj18 <oj@oojmed.com>
2021-07-27 12:27:58 +0100

    [CI] build: add commit hash and date to preface

--- building/generateDevChangelog.sh
+++ building/generateDevChangelog.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-preface="## Commits this release"
+preface="### Based on commit $(git rev-parse HEAD | cut -c1-7), $(date "+%d/%m %H:%M")\n## Commits this release"
 list="$(git log --pretty="format:%s" | grep -m 1 -B 100 "\[Release Prep\]" | head -n -1 | awk '$0="  - "$0')"
 
 echo -en "$preface\n$list" > dist/devChangelog.md
