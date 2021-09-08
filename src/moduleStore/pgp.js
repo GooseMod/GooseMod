cfa94d0a897af1a84e7e19c41abb03952833aa5b
Oj18 <oj@oojmed.com>
2021-09-08 19:57:02 +0100

    [Store > PGP] Force openpgp Node check to false

--- src/moduleStore/pgp.js
+++ src/moduleStore/pgp.js
@@ -2,7 +2,9 @@ let openpgp = undefined;
 
 // Dynamically load library as bundle size dramatically (3x) increases if we just import / use with NPM
 const loadLibrary = async () => {
-  const js = await (await fetch(`https://api.goosemod.com/pgp.js`, { cache: 'force-cache' })).text();
+  let js = await (await fetch(`https://api.goosemod.com/pgp.js`, { cache: 'force-cache' })).text();
+
+  js = js.replace('detectNode:function(){', 'detectNode:function(){return false;');
 
   openpgp = (eval(js + ';openpgp'));
 };
